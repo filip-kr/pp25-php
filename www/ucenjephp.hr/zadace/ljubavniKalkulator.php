305d1bcdd62254a510070594edcd9cae7fef7433
Građanin F <filipkr@protonmail.com>
2022-07-17 20:19:51 +0200

    Ljubavni kalkulator početak

---
+++ www/ucenjephp.hr/zadace/ljubavniKalkulator.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en" dir="ltr">
+
+<head>
+    <?php require_once '../head.php'; ?>
+</head>
+
+<body>
+    <!-- START zagljavlje -->
+    <header>
+        <?php require_once '../zaglavlje.php'; ?>
+    </header>
+    <!-- END zaglavlje -->
+
+    <!-- START tijelo -->
+    <main>
+        <div class="callout">
+            <div class="gornji-tekst">
+                <h5>
+                    <b>LJUBAVNI KALKULATOR:</b> unesite dva imena i izračunajte
+                    koliko ima ljubavi u zraku
+                </h5>
+            </div>
+        </div>
+        <div class="callout" id="izlaz">
+            <div class="grid-container fluid">
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-4">
+                        <form action="" method="get">
+                            <label for="ime1">
+                                IME 1:
+                                <input type="text" name="ime1" required>
+                            </label>
+
+                            <label for="ime2">
+                                IME 2:
+                                <input type="text" name="ime2" required>
+                            </label>
+
+                            <label for="izracunaj">
+                                <input type="submit" name="izracunaj" value="Izračunaj" class="button">
+                            </label>
+                        </form>
+                    </div>
+                    <div class="cell small-4">
+                        <?php require_once 'ljubavniKalkulatorRequire.php' ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- END tijelo -->
+
+    <!-- START podnožje -->
+    <footer>
+        <?php require_once '../podnozje.php'; ?>
+    </footer>
+    <!-- END podnožje -->
+
+
+    <!-- JavaScript -->
+    <?php require_once '../js.php'; ?>
+</body>
+
+</html>
---
+++ www/ucenjephp.hr/zadace/ljubavniKalkulatorRequire.php
@@ -0,0 +1,23 @@
+<?php
+
+$ime1 = 'Filip';
+$ime1Duzina = strlen($ime1);
+$ime2 = 'Tea';
+$ime2Duzina = strlen($ime2);
+
+$imena = $ime1 . $ime2;
+$imena = strtolower($imena);
+$imena = str_replace(' ', '', $imena);
+$imena = str_split($imena);
+$imena2 = array_count_values($imena);
+
+$ponovljenaSlova = [];
+
+for ($i = 0; $i < count($imena); $i++) {
+    if (array_key_exists($imena[$i], $imena2)) {
+        $ponovljenaSlova[] = $imena2[$imena[$i]];
+    }
+}
+
+echo '<pre />';
+print_r($ponovljenaSlova);
